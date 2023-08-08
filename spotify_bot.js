8a399d1e0c3ba89a16a036ba43921a1ddf398fd7
DennisHeijnen <heijnend@gmail.com>
2023-08-08 10:57:23 +0200

    Add port

--- spotify_bot.js
+++ spotify_bot.js
@@ -16,7 +16,7 @@ require('dotenv').config();
 var authOptions = {
   url: 'https://accounts.spotify.com/api/token',
   headers: {
-    'Authorization': 'Basic ' + (new Buffer(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET).toString('base64'))
+    'Authorization': 'Basic ' + (Buffer.from(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET).toString('base64'))
   },
   form: {
     grant_type: 'client_credentials'
